8f7fc7005be05d3485bfab13887076c438700b10
yael <howitzer117@outlook.com>
2025-11-06 10:06:13 -0600

    ajuste de las rutas relativas

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,49 @@
 
 
-// vite.config.js
+// // vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+  
+//   // 👇 importante para servir desde Express correctamente
+//   base: './', // rutas relativas
+  
+//   build: {
+//     outDir: 'dist', // carpeta donde se genera el build
+//     emptyOutDir: true,
+//   },
+
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
+
+
+
+
+
+
+
+
+
+
+// // vite.config.js
+
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+// // import tailwindcss from '@tailwindcss/vite'
+
+// // export default defineConfig({
+// //   plugins: [react(), tailwindcss()],
+// // })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -9,11 +52,12 @@ import path from 'path'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   
-  // 👇 importante para servir desde Express correctamente
-  base: './', // rutas relativas
+  // SOLUCIÓN: Usar la ruta absoluta del subdirectorio en producción.
+  // Esto fuerza al navegador a buscar los assets SIEMPRE desde la raíz de /admin/.
+  base: '/admin/', 
   
   build: {
-    outDir: 'dist', // carpeta donde se genera el build
+    outDir: 'dist', 
     emptyOutDir: true,
   },
 
@@ -23,22 +67,3 @@ export default defineConfig({
     },
   },
 })
-
-
-
-
-
-
-
-
-
-
-// vite.config.js
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// })
