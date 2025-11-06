c7210c6c5c786b4fde1e2f26ba766f0ae06b5110
yael <howitzer117@outlook.com>
2025-11-06 10:11:17 -0600

    ajuste de las rutas relativas 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,28 @@
 
 
-// // vite.config.js
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// import path from 'path'
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
   
-//   // 👇 importante para servir desde Express correctamente
-//   base: './', // rutas relativas
+  // 👇 importante para servir desde Express correctamente
+  base: './', // rutas relativas
   
-//   build: {
-//     outDir: 'dist', // carpeta donde se genera el build
-//     emptyOutDir: true,
-//   },
+  build: {
+    outDir: 'dist', // carpeta donde se genera el build
+    emptyOutDir: true,
+  },
 
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// })
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
 
 
 
@@ -44,26 +44,3 @@
 // // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  
-  // SOLUCIÓN: Usar la ruta absoluta del subdirectorio en producción.
-  // Esto fuerza al navegador a buscar los assets SIEMPRE desde la raíz de /admin/.
-  base: '/admin/', 
-  
-  build: {
-    outDir: 'dist', 
-    emptyOutDir: true,
-  },
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
